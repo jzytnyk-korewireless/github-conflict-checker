04fd84bb601be5855046874ab551fdcd05cdf3f9
David Alpert <dalpert@korewireless.com>
2016-08-30 12:32:43 -0500

    initial checkin

---
+++ GitHubConflictChecker/GitHubConflictChecker/Program.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp;
+
+namespace GitHubConflictChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient(@"https://api.github.com");
+
+            var request = new RestRequest("/repos/{owner}/{repo}/pulls", Method.POST);
+            request.AddUrlSegment("owner", "RacoWireless"); // replaces matching token in request.Resource
+            request.AddParameter("name", "value"); // adds to POST or URL querystring based on Method
+        }
+    }
+}
