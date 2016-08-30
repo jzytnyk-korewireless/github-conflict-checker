bbabc6322fda98cf9859984cf21556c4e3a91a32
David Alpert <dalpert@korewireless.com>
2016-08-30 13:27:47 -0500

    gets PR details and the mergable flag

--- GitHubConflictChecker/GitHubConflictChecker/Program.cs
+++ GitHubConflictChecker/GitHubConflictChecker/Program.cs
@@ -1,4 +1,7 @@
-﻿using RestSharp;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using RestSharp;
 
 namespace GitHubConflictChecker
 {
@@ -12,8 +15,36 @@ namespace GitHubConflictChecker
             request.AddUrlSegment("owner", "dalpert-korewireless"); // replaces matching token in request.Resource
             request.AddUrlSegment("repo", "github-conflict-checker"); // replaces matching token in request.Resource
 
-            IRestResponse response = client.Execute(request);
-            var content = response.Content; 
+            IRestResponse<List<Pulls>> response = client.Execute<List<Pulls>>(request);
+            var content = response.Content;
+
+            var pullRequestDetails = response.Data.Select(pr =>
+            {
+                var request2 = new RestRequest("/repos/{owner}/{repo}/pulls/{number}");
+
+                request2.AddUrlSegment("owner", "dalpert-korewireless"); // replaces matching token in request.Resource
+                request2.AddUrlSegment("repo", "github-conflict-checker");
+                // replaces matching token in request.Resource
+                request2.AddUrlSegment("number", pr.Number.ToString());
+
+                var detailsResponse = client.Execute<PullRequestDetail>(request2);
+
+                return detailsResponse.Data;
+            })
+                .ToList();
+
+            //GET /repos/:owner/:repo/pulls/:number
         }
+    }
+
+    internal class PullRequestDetail
+    {
+        public bool Mergeable { get; set; }
+    }
+
+    public class Pulls
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
     }
 }
