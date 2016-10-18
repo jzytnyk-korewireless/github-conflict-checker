edeee9aded3048ecd20322f1c27849f224edbdc7
Quan Dai <qdai@koresystemsgroup.com>
2016-10-18 13:27:25 -0500

    work in progress- preventing duplicate comments

--- GitHubConflictChecker/GitHubConflictChecker/Program.cs
+++ GitHubConflictChecker/GitHubConflictChecker/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Messaging;
 using RestSharp;
 
 namespace GitHubConflictChecker
@@ -13,6 +14,9 @@ namespace GitHubConflictChecker
             var apiKey = File.ReadAllText("ApiKey.txt");
             return apiKey;
         }
+
+        private static string prUnMergeableComment = "This PR is unmergeable!";
+
         static void Main(string[] args)
         {
             var apiKey = getApiKey();
@@ -37,21 +41,49 @@ namespace GitHubConflictChecker
 
                 if (pullRequestDetail.Mergeable == false)
                 {
-                    var request3 = new RestRequest("/repos/{owner}/{repo}/issues/{number}/comments", Method.POST);
-                    request3.AddParameter("Authorization", "token " + apiKey,ParameterType.HttpHeader);
-                    request3.AddUrlSegment("owner", "dalpert-korewireless"); // replaces matching token in request.Resource
-                    request3.AddUrlSegment("repo", "github-conflict-checker"); // replaces matching token in request.Resource
-                    request3.AddUrlSegment("number", pr.Number.ToString());
-                    request3.RequestFormat = DataFormat.Json;
-                    request3.AddBody(new { body = string.Format("There is a PR that is unmergeable! @{0}",pullRequestDetail.User.Login) });
-                    var response3 = client.Execute(request3);
+                    var getComments = new RestRequest("/repos/{owner}/{repo}/pulls/{number}/comments", Method.GET);
+                    getComments.AddParameter("Authorization", "token " + apiKey, ParameterType.HttpHeader);
+                    getComments.AddUrlSegment("owner", "dalpert-korewireless"); // replaces matching token in request.Resource
+                    getComments.AddUrlSegment("repo", "github-conflict-checker"); // replaces matching token in request.Resource
+                    getComments.AddUrlSegment("number", pr.Number.ToString());
+                    getComments.RequestFormat = DataFormat.Json;
+                    var comments = client.Execute(getComments);
+
+                    if (false)
+                    {
+                        var request3 = new RestRequest("/repos/{owner}/{repo}/issues/{number}/comments", Method.POST);
+                        request3.AddParameter("Authorization", "token " + apiKey, ParameterType.HttpHeader);
+                        request3.AddUrlSegment("owner", "dalpert-korewireless"); // replaces matching token in request.Resource
+                        request3.AddUrlSegment("repo", "github-conflict-checker"); // replaces matching token in request.Resource
+                        request3.AddUrlSegment("number", pr.Number.ToString());
+                        request3.RequestFormat = DataFormat.Json;
+                        request3.AddBody(new { body = string.Format("{0} @{1}", prUnMergeableComment, pullRequestDetail.User.Login) });
+                        var response3 = client.Execute(request3);            
+                    }
+
+
                 }
 
                 return pullRequestDetail;
             })
                 .ToList();
         }
+        //private static bool NeedToBeNotified(PullRequestDetail prDetail, Pulls pr)
+        //{
+        //    var apiKey = getApiKey();
+        //    var request = new RestRequest("/repos/{owner}/{repo}/issues/{number}/comments", Method.GET);
+        //    request.AddParameter("Authorization", "token " + apiKey, ParameterType.HttpHeader);
+        //    request.AddUrlSegment("owner", "dalpert-korewireless"); // replaces matching token in request.Resource
+        //    request.AddUrlSegment("repo", "github-conflict-checker"); // replaces matching token in request.Resource
+        //    request.AddUrlSegment("number", pr.Number.ToString());
+        //    request.RequestFormat = DataFormat.Json;
+        //    var response = client.Execute(request);  
+
+        //    return false;
+        //}
     }
+
+
     internal class User
     {
         public string Login{ get; set; }
@@ -62,6 +94,10 @@ namespace GitHubConflictChecker
         public User User { get; set; }
     }
 
+    internal class Comment
+    {
+        public int Id { get; set; }
+    }
     public class Pulls
     {
         public int Id { get; set; }
